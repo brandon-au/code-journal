76a88338fa9156e887be2b57dfed05fb4c8e42a5
Brandon Au <design.brandonau@gmail.com>
2022-09-14 17:25:23 +0000

    Completed local storage functionality and submit event on entry form. #1

--- js/main.js
+++ js/main.js
@@ -3,9 +3,22 @@ var $photoUpload = document.querySelector('#photo');
 // https://miro.medium.com/max/2400/1*Csahvngj_g332RhhgD7OPQ.jpeg
 // image link for testing
 
-$photoUpload.addEventListener('input', imagePreview);
-
 var $image = document.querySelector('#preview');
-function imagePreview(event) {
+$photoUpload.addEventListener('input', function () {
   $image.setAttribute('src', event.target.value);
+});
+
+var $journalEntry = document.querySelector('#journal-entry');
+var $submit = document.querySelector('.submit-btn');
+$submit.addEventListener('submit', submitData);
+
+function submitData() {
+  var entryData = {
+    Title: $journalEntry.elements.Title.value,
+    $image,
+    Notes: $journalEntry.elements.Notes.value,
+    entryId: data.nextEntryId
+  };
+  data.nextEntryId.value++;
+  $journalEntry.prepend(entryData);
 }
