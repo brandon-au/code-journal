13bc5e4775e6ddccf7f4598df2ab71725865f0b0
Brandon Au <design.brandonau@gmail.com>
2022-09-14 23:21:47 +0000

    Completed local storage functionality revised submit event. #1

--- js/data.js
+++ js/data.js
@@ -6,3 +6,14 @@ var data = {
   editing: null,
   nextEntryId: 1
 };
+
+var dataJSON = localStorage.getItem('journal-entry-storage');
+if (dataJSON !== null) {
+  data = JSON.parse(dataJSON);
+}
+
+window.addEventListener('beforeunload', function () {
+  var entries = JSON.stringify(data);
+  localStorage.setItem('journal-entry-storage', entries);
+}
+);
--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@ var $photoUpload = document.querySelector('#photo');
 
 // https://miro.medium.com/max/2400/1*Csahvngj_g332RhhgD7OPQ.jpeg
 // image link for testing
+// Title: Ada Lovelace
+// Notes: Augusta Ada King, Countess of Lovelace was an English mathematician and writer, chiefly known for her work on Charles Babbage's proposed mechanical general-purpose computer, the Analytical Engine.
 
 var $image = document.querySelector('#preview');
 $photoUpload.addEventListener('input', function () {
@@ -9,16 +11,18 @@ $photoUpload.addEventListener('input', function () {
 });
 
 var $journalEntry = document.querySelector('#journal-entry');
-var $submit = document.querySelector('.submit-btn');
-$submit.addEventListener('submit', submitData);
+$journalEntry.addEventListener('submit', submitData);
 
-function submitData() {
+function submitData(event) {
+  event.preventDefault();
   var entryData = {
-    Title: $journalEntry.elements.Title.value,
-    $image,
-    Notes: $journalEntry.elements.Notes.value,
+    Title: $journalEntry.elements.title.value,
+    Image: $journalEntry.elements.PhotoURL.value,
+    Notes: $journalEntry.elements.notes.value,
     entryId: data.nextEntryId
   };
-  data.nextEntryId.value++;
-  $journalEntry.prepend(entryData);
+  data.nextEntryId++;
+  data.entries.unshift(entryData);
+  $image.setAttribute('src', '/images/placeholder-image-square.jpg');
+  $journalEntry.reset();
 }
